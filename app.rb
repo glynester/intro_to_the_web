ff51584770648764e164649fbc70c43fc6307b31
Glenn <glynester@hotmail.com>
2016-10-10 14:45:24 +0000

    Added a para

--- app.rb
+++ app.rb
@@ -5,8 +5,13 @@ set :session_secret, 'super secret'
 get '/cat' do
   "<div style='border: 3px dashed red'>
      <img src='http://bit.ly/1eze8aE'>
-   </div>"
+   </div>
+   <div>
+      <p>Soft kitty warm kitty purr purr purr</p>
+    <div>"
 end
+
+
 
 get '/secret' do
   'This is a secret page'
