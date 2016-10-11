f11fe0e1ace341d0cc5eb1ee67bbb358b8395318
Glenn <glynester@hotmail.com>
2016-10-11 09:39:40 +0000

    Step 14 Post method.

--- app.rb
+++ app.rb
@@ -7,11 +7,16 @@ get '/random-cat' do
  erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
 end
+
+get '/cat-form' do
+ erb(:cat_form)
+end
+
 
 get '/' do
   "Hello World"
